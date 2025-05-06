20fd5961d764d50cd63333dce4b99cd32aeabeb9
Daniel Sadiq <danielsadiq93@gmail.com>
2025-05-06 15:19:16 +0100

    comps and pages add

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import Product from "./pages/Product"
-import Pricing from "./pages/Pricing"
-import Homepage from "./pages/Homepage"
-import PageNotFound from "./pages/PageNotFound"
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Product from "./pages/Product";
+import Pricing from "./pages/Pricing";
+import Homepage from "./pages/Homepage";
+import PageNotFound from "./pages/PageNotFound";
+import AppLayout from "./pages/AppLayout";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
         <Route path="/" element={<Homepage/>}/>
         <Route path="product" element={<Product/>} />
         <Route path="pricing" element={<Pricing/> }/>
+        <Route path="/login" element={<Login/> }/>
+        <Route path="app" element={<AppLayout/> }/>
         <Route path="*" element={<PageNotFound/>}/>
       </Routes>
     </BrowserRouter>
---
+++ src/pages/AppLayout.tsx
@@ -0,0 +1,12 @@
+import Sidebar from "../components/Sidebar"
+
+function AppLayout() {
+  return (
+    <div>
+      <Sidebar/>
+      APP
+    </div>
+  )
+}
+
+export default AppLayout
