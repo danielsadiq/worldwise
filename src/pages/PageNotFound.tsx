d285efde5a87016c9498bcc0e62d4ddd9c022794
Daniel Sadiq <danielsadiq93@gmail.com>
2025-05-06 13:38:07 +0100

    pages add

---
+++ src/pages/PageNotFound.tsx
@@ -0,0 +1,12 @@
+import PageNav from "../components/PageNav"
+
+function PageNotFound() {
+  return (
+    <h1>
+      <PageNav/>
+      Page not found :(
+    </h1>
+  )
+}
+
+export default PageNotFound
